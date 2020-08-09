aad5116a08394df00d80812366aef34935684bab
Dymek <d.rzepiejewski@gmail.com>
2020-08-09 22:27:18 +0200

    Week2Exercise 8 start

--- Week2Exercise8/Program.cs
+++ Week2Exercise8/Program.cs
@@ -6,7 +6,25 @@ namespace Week2Exercise8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Week2Exercise8!");
+
+            Exe1();
         }
-    }
+
+        static public void Exe1()
+        {
+            Console.WriteLine("Ile jest licz pierwszych w przedziale 0 - 100");
+            int count = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i== 0 || i ==1)
+                {
+                    continue;
+                }
+
+
+            }
+
+        }
+}
 }
